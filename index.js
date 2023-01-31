cde3cd49086e6e9bc806b175e259d50f1d94ca53
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-01-31 10:06:03 +0800

    initial js code copying

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+chatGPT generated code
+
+const form = document.querySelector('#lawn-info');
+const scheduleDiv = document.querySelector('#schedule');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const lawnSize = parseInt(document.querySelector('#lawn-size').value);
+  const mowInterval = parseInt(document.querySelector('#mow-interval').value);
+
+  const schedule = `Your lawn should be mowed every ${mowInterval} weeks.`;
+
+  scheduleDiv.textContent = schedule;
+});
+
+*/
+
+const form = document.querySelector('#lawn-info')
