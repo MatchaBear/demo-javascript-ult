760955d951a2441186c498a00b6af7ed47d393d9
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-01 00:52:33 +0800

    ult homepage modified

---
+++ cut-grass-app/index.js
@@ -0,0 +1,16 @@
+const typeOfPlant = [
+  "Decorative plant",
+  "Food plant",
+  "Industrial plant",
+  "Utility plant",
+];
+
+const atmosphere = [
+  "Carbon dioxide",
+  "Chlorine",
+  "Oxygen",
+  "Water",
+  "Polluted water",
+];
+
+function illumination()
